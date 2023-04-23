25f89cb6a7b28873ba4afc2e8f5892b2eae280b6
fernando predes <fernando_predes@hotmail.com>
2023-04-23 17:18:22 -0300

    created login screen

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -1,6 +1,33 @@
 import styled from "styled-components"
 
 export const HomeStyle = styled.div `
-  background: red;
-
+text-align: center;
+h1{
+  font-family: 'Rawline';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 44px;
+  color: #0057A0;
+  margin: 55px 0 20px 0;
+}
+h2{
+  font-style: normal;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 55px;
+  color: #0074A6;
+  margin-bottom: 30px;
+}
+span{
+  font-style: normal;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 25px;
+  color: #003D77;
+  margin-top: 50px;
+}
+img{
+  margin-bottom: 90px;
+}
 `
